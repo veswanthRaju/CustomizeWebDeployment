44441cb1dcb37eab15a52b23bf6254acd301a157
Veswanth Raju <veswanthraju.thokachichu@walkingtree.tech>
2017-05-26 18:14:32 +0530

    Edited Program.cs file

--- AutomateWebDeployment/Program.cs
+++ AutomateWebDeployment/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutomateWebDeployment
 {
@@ -13,11 +9,11 @@ namespace AutomateWebDeployment
         {
             Console.WriteLine("Do you want to start WinScp application Y/N");
             var launchWinSCP = Console.ReadLine();
-            if (launchWinSCP == "y" || launchWinSCP == "Y")
+            if (launchWinSCP.ToLower() == "y")
             {
-                OpenExternalApplcation(@"C:\Windows\Sysnative\WinSCP.exe");
+                OpenExternalApplcation(@"C:\Program Files (x86)\WinSCP\WinSCP.exe", "winscp");
                 Console.WriteLine("WinSCP Launched..");
-                OpenExternalApplcation(@"C:\Windows\Sysnative\putty.exe");
+                OpenExternalApplcation(@"C:\Windows\Sysnative\putty.exe", "putty");
                 Console.WriteLine("Putty Launched..");
             }
             else
@@ -25,12 +21,35 @@ namespace AutomateWebDeployment
             Console.ReadLine();
         }
 
-        private static void OpenExternalApplcation(string appPath)
+        #region Private Methods
+        private static void OpenExternalApplcation(string appPath, string appName)
         {
-            ProcessStartInfo info = new ProcessStartInfo();
-            //You should need to place the exe file in the ‪C:\Windows\System32 Location
-            info.FileName = appPath;
-            Process p = Process.Start(info);
+            if (appName == "winscp")
+            {
+                //Giving arguments to the launched application
+                Process p = Process.Start(appPath, "10.10.0.177 2222 gumstix gumstix");
+            }
+            else
+            {
+                Process p = Process.Start(appPath, "10.10.0.177 2222");
+            }
         }
+        #endregion
     }
+
+    #region Useful code
+
+    //OpenExternalApplcation(@"C:\Program Files\Dell SonicWALL\Global VPN Client\SWGVC.exe");
+    //Console.WriteLine("VPN client Launched..");
+
+    //ProcessStartInfo info = new ProcessStartInfo();
+    //You should need to place the .exe file in the ‪C:\Windows\System32 Location
+    //info.FileName = appPath;
+
+    //info.Arguments = "olaa"; //argument
+    //info.UseShellExecute = false;
+    //info.RedirectStandardOutput = true;
+    //info.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+    //info.CreateNoWindow = true; //not diplay a windows
+    #endregion
 }
